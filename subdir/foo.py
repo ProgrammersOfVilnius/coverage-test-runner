80e2285c030eb53f0f39603c83fa064f52a4ad66
Lars Wirzenius <liw@iki.fi>
2007-11-11 04:51:41 +0200

    Added sample files.

---
+++ subdir/foo.py
@@ -0,0 +1,9 @@
+class Foo:
+
+    def foo(self, a):
+        if a:
+            return True
+        else:
+            return False
+    
+    
---
+++ testrun.py
@@ -0,0 +1,5 @@
+from CoverageTestRunner import CoverageTestRunner
+
+r = CoverageTestRunner()
+r.find_pairs(".")
+r.run()

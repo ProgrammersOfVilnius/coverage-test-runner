9bacfa9fec9dcfffc9643ae674405d03f9835ab1
Lars Wirzenius <liw@liw.fi>
2009-11-18 19:46:13 +0200

    Updated setup.py for new version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='CoverageTestRunner',
-      version='1.1',
+      version='1.2',
       description='fail Python program unit tests unless they test everything',
       long_description='''\
 CoverageTestRunner runs unit tests implemented using the unittest module
@@ -9,18 +9,8 @@ in the Python standard library. It runs them using coverage.py and fails
 the test if all statements are not covered.
 ''',
       author='Lars Wirzenius',
-      author_email='liw@iki.fi',
+      author_email='liw@liw.fi',
       url='http://braawi.org/coveragetestrunner.html',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Testing',
-      ],
       license='GNU General Public License, version 2 or later',
       py_modules=['CoverageTestRunner'],
      )

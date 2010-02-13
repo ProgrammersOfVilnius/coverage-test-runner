7eed2eb7a22d0a121cc3a55dba1ba8874dced760
Lars Wirzenius <liw@liw.fi>
2010-02-14 12:24:30 +1300

    Release version 1.3.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='CoverageTestRunner',
-      version='1.2',
+      version='1.3',
       description='run unittests',
       author='Lars Wirzenius',
       author_email='liw@liw.fi',
